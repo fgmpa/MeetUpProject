509f5cd40c1834414553ca9fb0f31ff775cdc099
fgmpa <sherbakov_ev@bk.ru>
2025-08-30 18:55:55 +0400

    коррекции кэш

--- app/src/main/java/com/eugene/meetupproject/data/repository/EventRepositoryImpl.kt
+++ app/src/main/java/com/eugene/meetupproject/data/repository/EventRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.eugene.meetupproject.data.mapper.EventMap
 import com.eugene.meetupproject.domain.model.Event
 import com.eugene.meetupproject.domain.repository.EventRepository
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
 import kotlinx.coroutines.tasks.await
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -39,29 +40,30 @@ class EventRepositoryImpl @Inject constructor(
 
     override suspend fun getEventsForDay(date: String): Result<List<Event>> {
         return try {
-            val dayList = firestore.collection("events").get().await()
-            val allEvents = dayList.documents.map { it.toEvent() }
+            // сначала пробуем взять из кэша
+            val snapshot = firestore.collection("events").get(Source.CACHE).await()
 
-
+            val allEvents = snapshot.documents.map { it.toEvent() }
             val selectedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
 
             val events = allEvents.filter { event ->
-
                 val startDateTime = LocalDateTime.parse(event.dateStart, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
                 val endDateTime = LocalDateTime.parse(event.dateEnd, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
-
-
                 val startDate = startDateTime.toLocalDate()
                 val endDate = endDateTime.toLocalDate()
-
-
                 (selectedDate.isEqual(startDate) || selectedDate.isAfter(startDate)) &&
                         (selectedDate.isEqual(endDate) || selectedDate.isBefore(endDate))
             }
 
             Result.success(events)
         } catch (e: Exception) {
-            Result.failure(e)
+            try {
+                val snapshot = firestore.collection("events").get(Source.SERVER).await()
+                val allEvents = snapshot.documents.map { it.toEvent() }
+                Result.success(allEvents)
+            } catch (ex: Exception) {
+                Result.failure(ex)
+            }
         }
     }
     override suspend fun getEventsForUser(userId: String): Result<List<Event>> {
--- app/src/main/java/com/eugene/meetupproject/di/MyApp.kt
+++ app/src/main/java/com/eugene/meetupproject/di/MyApp.kt
@@ -2,12 +2,18 @@ package com.eugene.meetupproject.di
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MyApp : Application() {
     lateinit var appComponent: AppComponent
 
     override fun onCreate() {
         super.onCreate()
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true) // включает оффлайн режим
+            .build()
+        FirebaseFirestore.getInstance().firestoreSettings = settings
         FirebaseApp.initializeApp(this)
         appComponent = DaggerAppComponent.factory().create(this)
     }
